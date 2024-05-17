416dc41d4777ffda7a8f0ab755ff392148fb4136
funzter <funzter@mail.timesnake.de>
2024-05-17 21:41:53 +0200

    minor

--- src/main/java/de/timesnake/game/trapduel/chat/Plugin.java
+++ src/main/java/de/timesnake/game/trapduel/chat/Plugin.java
@@ -4,17 +4,11 @@
 
 package de.timesnake.game.trapduel.chat;
 
-import de.timesnake.library.basic.util.LogHelper;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-  public static final Plugin TRAP_DUEL = new Plugin("TrapDuel", "TRD",
-      LogHelper.getLogger("TrapDuel", Level.INFO));
+  public static final Plugin TRAP_DUEL = new Plugin("TrapDuel", "TRD");
 
-  protected Plugin(String name, String code, Logger logger) {
+  protected Plugin(String name, String code) {
     super(name, code);
   }
 }
